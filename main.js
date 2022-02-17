4f17ecb3397be23c67253ab0c20447738c8c7895
itspreetycute <94111188+itspreetycute@users.noreply.github.com>
2022-02-17 15:38:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+  video.size(600,300);
+
+video.parent('game_console');
+poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	  console.log("noseX = " + noseX +", noseY = " + noseY);
+	}
+  }
+
+
+
+
